c106fbe2d03f5e8d48770de235aa026d9dc8245d
Boa Matule <boa.matule@gmail.com>
2019-06-05 22:41:13 +0200

    Installs netlify contact form and style the contact form; Contact form still not sending the info. WIP

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,11 +1,41 @@
 import React from "react"
+import NetlifyForm from 'react-netlify-form'
 
 const Contact = () => {
   return (
-    <div>
-      <h1 className="content-text">Contact</h1>
-      <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    </div>)
+    <div class="container background-color: #553c9a  mx-auto px-4 .bg-black max-width: 500px">
+
+      <h1 align="center">Get in touch!</h1> 
+
+
+    <NetlifyForm name='Contact Form'>
+      {({ loading, error, success }) => (
+        <div>
+          {loading &&
+            <div>Loading...</div>
+          }
+          {error &&
+            <div>Your information was not sent. Please try again later.</div>
+          }
+          {success &&
+            <div>Thank you for contacting us!</div>
+          }
+          {!loading && !success &&
+            <div>
+              <div className="flex items-center justify-center">
+                <input className="shadow appearance-none border none w-full py-1 px-1 text-grey-darker leading-tight focus:outline-none" type="text"name="email" required />
+              </div>
+
+            <textarea className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker mb-3 leading-tight focus:outline-none" name="message" required/>
+            <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 none focus:outline-none focus:shadow-outline justify-center" type="submit">Submit</button>
+          </div>
+          }
+        </div>
+      )}
+  </NetlifyForm>
+
+  </div>
+  )
 }
 
 export default Contact 
