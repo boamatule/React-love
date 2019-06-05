3b2d19da12982520944963f2058b1e52d7caefc3
Boa Matule <boa.matule@gmail.com>
2019-06-05 14:14:12 +0200

    Installs unDraw... More styling.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,48 +1,62 @@
-import React, { Component} from "react"
+import React, { Component } from "react"
 import axios from "axios"
+import ProjectCard from "./ProjectCard"
+import { UndrawDashboard } from "react-undraw-illustrations"; 
+
+
 class Projects extends Component {
-  constructor() {
-    super();
-    this.state = {
-      projects: []
-    };
-  }
-  
-  componentDidMount() {
-    axios.get('./src/data/projects.json')
-        .then(response => {
-            this.setState({
-                projects: response.data
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        };
+    }
+    
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+            .then(response => {
+                this.setState({
+                    projects: response.data
+                });
             })
-        })
-}
+            
+    }
 
-  
     render() {
-      const projects = this.state.projects
-      let projectsList
+        const projects = this.state.projects
+        let projectsList 
 
-      if (projects.length > 0) {
-          projectsList = projects.map(project => {
-            return (
-              <div key={project.id}>
-                <h3 className="content-text">
-                  {project.name}
-                </h3>
-              </div>
-            )
-        })
-      }
+        if (projects.length > 0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                            <ProjectCard project={project} />
+                    </div>
+                )
+            })
+        }
 
-    return ( 
-        <div>
-            <h1 className="content-text">My Projects</h1>
-            {projectsList}
-        </div>
-      )     
+        return (
+          <div className="content-wrapper">
+                <div className="flex mb-4">
+                    <div className="w-1/4">
+                        <UndrawDashboard />
+                    </div>
+                    <div className="w-3/4">
+                        <h1>My Projects</h1>
+                        <p>This is a selection of some of my projects I have been working on.</p>
+                    </div>
+    
+                </div>
+            
+    
+                <div className="flex flex-wrap -mx-1 lg:-mx-4">
+                    {projectsList}
+                </div>
+            </div>
+        )
+    
     }
 };
-
-
 
 export default Projects
