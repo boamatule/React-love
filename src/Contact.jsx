cfe5e633c1e6cfbdbe43331b8c5afded186d8389
Boa Matule <boa.matule@gmail.com>
2019-06-07 15:06:21 +0200

    Still can't save the contacts. WIP

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,30 +1,44 @@
 import React from "react"
+import NetlifyForm from 'react-netlify-form'
 
 const Contact = () => {
   return (
-    <div class="container mx-auto px-4 .bg-black max-width: 500px">
+    <NetlifyForm name='Contact'>
+      {({ loading, error, success }) => (
+        <div>
+          {loading &&
+            <div>Loading...</div>
+          }
+          {error &&
+            <div>Your information was not sent. Please try again later.</div>
+          }
+          {success &&
+            <div>Thank you for contacting us!</div>
+          }
+          {!loading && !success &&
+                <div>  
+                    <h1 className="content-header" align="center">Get in touch</h1>
+                    <div className="flex items-center justify-center">
+                        <input className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker leading-tight focus:outline-none" type="text"name="name" placeholder="Name" required />
+                    </div>
 
-      <h1 align="center">Get in touch!</h1> 
 
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-          <input type="hidden" name="form-name" value="contact" />
+                      <div className="flex items-center justify-center">
+                        <input className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker leading-tight focus:outline-none" type="text"name="email" placeholder="Email" required />
+                      </div>
 
-              <div className="flex items-center justify-center">
-                <input className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker leading-tight focus:outline-none" type="text"name="name" placeholder="Name" required />
-              </div>
-
-
-              <div className="flex items-center justify-center">
-                <input className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker leading-tight focus:outline-none" type="text"name="email" placeholder="Email" required />
-              </div>
-
-            <div>
-            <textarea className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker mb-3 leading-tight focus:outline-none" name="message" placeholder="Message"required/>
-            <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 none focus:outline-none focus:shadow-outline justify-center" type="submit">Submit</button>
-          </div>
-      </form>
-    </div> 
+                      <div>
+                        <textarea className="shadow appearance-none border none w-full py-2 px-1 text-grey-darker mb-3 leading-tight focus:outline-none" name="message" placeholder="Message"required/>
+                        <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 none focus:outline-none focus:shadow-outline justify-center" type="submit">Submit</button>
+                      </div>
+                </div>
+          }
+        </div>
+      )}
+    </NetlifyForm>
   )
 }
 
-export default Contact 
+export default Contact
+
+
