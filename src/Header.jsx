0cccfedd09d8d58ee512c0589ecfb595ce87a0f3
Boa Matule <boa.matule@gmail.com>
2019-06-15 16:34:21 +0200

    Adds more component to the App + styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,18 @@
 import { NavLink, Link } from 'react-router-dom';
 import React from "react"
 
+
 const Header = () => {
     return (
-        <nav className="header">
-            <h1 className="header-title">
-                <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
-            </h1>
+        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-10">                 
+          <h1 className="header-title">
+            <Link className="text-white hover:text-grey no-underline" to='/projects'>My Portfolio</Link>
+          </h1>
             <ul className="list-reset flex">
                 <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
                 <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/contact'>Contact Me</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/resume'>My CV</NavLink></li>
             </ul>
         </nav>
     )
