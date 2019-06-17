8d789e9827e55cfe303d718a80478232fc295d4f
Boa Matule <boa.matule@gmail.com>
2019-06-17 15:58:35 +0200

    Adds react icon/link in the Footer component.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,34 +1,38 @@
 import React from "react"
 import NetlifyForm from 'react-netlify-form'
+import { Container, Form, Input, Button } from 'semantic-ui-react'
 
 const Contact = () => {
   return (
-    <NetlifyForm name='Contact'>
-      {({ loading, error, success }) => (
-        <div>
-          {loading &&
-            <div>Loading...</div>
-          }
-          {error &&
-            <div>Your information was not sent. Please try again later.</div>
-          }
-          {success &&
-            <div>Thank you for contacting us!</div>
-          }
-          {!loading && !success &&
-            <>
-              <div className="contact-form-wrapper">
-                  <input className="input-field-sm" type='text' placeholder="Your name" name='name' required />
 
-                  <input class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com" required />
-                  <textarea className="input-field" placeholder="Your message" name='message' required />
-                  <button className="button">Get in touch!</button>
-              </div>
-            </>
-        }
-      </div>
-      )}
-    </NetlifyForm>
+    <Container>
+      <NetlifyForm name='Contact' method='POST' netlify-honeypot="bot-field" data-netlify="true">
+        {({ loading, error, success }) => (
+          <div>
+            {loading &&
+              <div>Loading...</div>
+            }
+            {error &&
+              <div>Your information was not sent. Please try again later.</div>
+            }
+            {success &&
+              <div>Thank you for contacting us!</div>
+            }
+            {!loading && !success &&
+              <>
+                <div>
+                    <input className="input-field-sm" type='text' placeholder="Your name" name='name' required />
+
+                    <input placeholder="jane@example.com" required />
+                    <Form>  <textarea placeholder="Tell us more" required /> </Form>
+                    <button class="ui button">Get in touch!</button>
+                </div>
+              </>
+          }
+        </div>
+        )}
+      </NetlifyForm>
+    </Container>
   )
 }
 export default Contact
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Footer = () => {
 	return (
 		<div class="border-t border-smoke px-8 py-4">
-      <div class="flex justify-center text-grey">
-        Made with React
+      <div class="justify-center">
+				<a href="https://reactjs.org/" target="_blank"><img src="/src/data/react.png" height="40" width="40" alt="React Logo"></img></a>
 			</div>
 		</div>
 	)
